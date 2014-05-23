9ed30938f6614bd3e3d8104c067be1a36e39c529
Temur Fatkulin <temur.fatkulin@gmail.com>
2014-05-24 04:09:26 +0600

    refactor methods and constants

--- lib/random_org_http_api/configuration.rb
+++ lib/random_org_http_api/configuration.rb
@@ -15,15 +15,15 @@ module RandomOrgHttpApi
 
     # Query templates
     # Integer
-    INTEGER_QUERY_PARAMS = [:num, :min, :max, :col, :base, :format, :rnd]
+    INTEGER_QUERY_KEYS = [:num, :min, :max, :col, :base, :format, :rnd]
     INTEGER_QUERY_TEMPLATE = "/integers/?num=%{num}&min=%{min}&max=%{max}&col=%{col}&base=%{base}&format=%{format}&rnd=%{rnd}"
 
     # String
-    STRING_QUERY_PARAMS = [:num, :len, :digits, :upperalpha, :loweralpha, :unique, :format, :rnd]
+    STRING_QUERY_KEYS = [:num, :len, :digits, :upperalpha, :loweralpha, :unique, :format, :rnd]
     STRING_QUERY_TEMPLATE = "/strings/?num=%{num}&len=%{len}&digits=%{digits}&upperalpha=%{upperalpha}&loweralpha=%{loweralpha}&unique=%{unique}&format=%{format}&rnd=%{rnd}"
 
     # Sequence
-    SEQUENCE_QUERY_PARAMS = [:min, :max, :col, :format, :rnd]
+    SEQUENCE_QUERY_KEYS = [:min, :max, :col, :format, :rnd]
     SEQUENCE_QUERY_TEMPLATE = "/sequences/?min=%{min}&max=%{max}&col=%{col}&format=%{format}&rnd=%{rnd}"
 
   end
--- lib/random_org_http_api/generator.rb
+++ lib/random_org_http_api/generator.rb
@@ -4,48 +4,57 @@ module RandomOrgHttpApi
     include RandomOrgHttpApi::Configuration
 
     def generate_integers(params = {})
-      request('integer', params)
+      integers_query = query('integer', params)
+      request(DOMAIN, integers_query)
     end
 
     def generate_strings(params = {})
-      request('string', params)
+      strings_query = query('string', params)
+      request(DOMAIN, strings_query)
     end
 
     def generate_sequence(params = {})
-      request('sequence', params)
+      sequence_query = query('sequence', params)
+      request(DOMAIN, sequence_query)
     end
 
     private
 
     # Метод запроса на сайт random.org
-    def request(object_type, params)
-      # В зависимости от типа получаемого объекта валидируем параметры и подставляем в нужный темплейт
-      case object_type
-        when 'integer' then
-          all_params = permit_params(DEFAULT_QUERY_PARAMS, params, INTEGER_QUERY_PARAMS)
-          query  = generate_query(all_params, INTEGER_QUERY_TEMPLATE)
-        when 'string' then
-          all_params = permit_params(DEFAULT_QUERY_PARAMS, params, STRING_QUERY_PARAMS)
-          query  = generate_query(all_params, STRING_QUERY_TEMPLATE)
-        when 'sequence' then
-          all_params = permit_params(DEFAULT_QUERY_PARAMS, params,  SEQUENCE_QUERY_PARAMS)
-          query  = generate_query(all_params, SEQUENCE_QUERY_TEMPLATE)
-      end
+    def request(domain, query)
       # Создаем запрос
-      res = Net::HTTP.get_response(DOMAIN, query)
+      res = Net::HTTP.get_response(domain, query)
       # Если все хорошо, то возвращаем форматированный ответ,
       # если нет, то показываем ответ ошибки сервера.
       res.is_a?(Net::HTTPSuccess) ? res.body.split : res.body.strip
     end
 
-    # Метод оставляющий только нужные параметры.
-    def permit_params(default_params, params, required_params )
-      @params = default_params.merge(params)
-      @params = @params.select {|k,v| required_params.include?(k) }
+    # Метод для генерации запроса
+    def query(object_type, params)
+      # Совмещаем параметры по умолчанию с параметами, который передал пользователь
+      all_params = DEFAULT_QUERY_PARAMS.merge(params)
+      # В зависимости от типа объекта, который нужно сгенерировать, оставляем только нужные для него параметры.
+      case object_type
+        when 'integer' then
+          right_params = permit_params(all_params, INTEGER_QUERY_KEYS)
+          insert_params(right_params, INTEGER_QUERY_TEMPLATE)
+        when 'string' then
+          right_params = permit_params(all_params, STRING_QUERY_KEYS)
+          insert_params(right_params, STRING_QUERY_TEMPLATE)
+        when 'sequence' then
+          right_params = permit_params(all_params, SEQUENCE_QUERY_KEYS)
+          insert_params(right_params, SEQUENCE_QUERY_TEMPLATE)
+      end
     end
 
-    # Метод подстановки параметров в темплейт. Параметр должен быть хешем.
-    def generate_query(params, template)
+    # Метод оставляющий только нужные параметры.
+    def permit_params(params, required_keys)
+      params.select { |k| required_keys.include?(k) }
+    end
+
+    # Метод подстановки параметров в темплейт.
+    # Параметры должны передаваться в хеше
+    def insert_params(params, template)
       template % params
     end
 
