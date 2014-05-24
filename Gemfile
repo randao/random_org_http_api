e2b208518c9103db4217746eb2790cb3f9b79d7e
Temur Fatkulin <temur.fatkulin@gmail.com>
2014-05-25 03:22:14 +0600

    add simplecov

--- Gemfile
+++ Gemfile
@@ -3,5 +3,8 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in random_org_http_api.gemspec
 gemspec
 
-gem 'coveralls', require: false
-gem 'codeclimate-test-reporter', group: :test, require: nil
+group :test do
+  gem 'simplecov', :require => false
+  gem 'codeclimate-test-reporter', require: nil
+  gem 'coveralls', require: false
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,13 @@ require 'bundler/setup'
 require 'random_org_http_api'
 require 'codeclimate-test-reporter'
 require 'coveralls'
+require 'simplecov'
 
-Coveralls.wear!
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter,
+    CodeClimate::TestReporter::Formatter
+]
+
+SimpleCov.start
 CodeClimate::TestReporter.start
