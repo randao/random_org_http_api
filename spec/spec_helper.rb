95dd64301cb8ba286ed4f4309645336c1b9de6d3
Temur Fatkulin <temur.fatkulin@gmail.com>
2014-05-25 03:07:45 +0600

    add codeclimate-test-reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'random_org_http_api'
-
+require 'codeclimate-test-reporter'
 require 'coveralls'
+
 Coveralls.wear!
+CodeClimate::TestReporter.start
