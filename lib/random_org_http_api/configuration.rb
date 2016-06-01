787145b96952d957d302163a508e643a9e044b4d
Temur Fatkulin <temur.fatkulin@gmail.com>
2016-06-01 15:02:50 +0500

    refactoring

--- lib/random_org_http_api/configuration.rb
+++ lib/random_org_http_api/configuration.rb
@@ -1,30 +1,32 @@
 module RandomOrgHttpApi
   module Configuration
-    DOMAIN = 'https://www.random.org'
+    BASE_URI = 'https://www.random.org'
+
     DEFAULT_QUERY_PARAMS = {
-        format: 'plain',
-        base: '10',
-        rnd: 'new',
-        col: '1',
-        min: '1',
-        digits: 'on',
-        loweralpha: 'on',
-        upperalpha: 'on',
-        unique: 'on'
+      format: 'plain',
+      base: '10',
+      rnd: 'new',
+      col: '1',
+      min: '1',
+      digits: 'on',
+      loweralpha: 'on',
+      upperalpha: 'on',
+      unique: 'on'
     }
 
-    # Query templates
-    # Integer
-    INTEGER_QUERY_KEYS = [:num, :min, :max, :col, :base, :format, :rnd]
-    INTEGER_QUERY_TEMPLATE = "integers/?num=%{num}&min=%{min}&max=%{max}&col=%{col}&base=%{base}&format=%{format}&rnd=%{rnd}"
-
-    # String
-    STRING_QUERY_KEYS = [:num, :len, :digits, :upperalpha, :loweralpha, :unique, :format, :rnd]
-    STRING_QUERY_TEMPLATE = "strings/?num=%{num}&len=%{len}&digits=%{digits}&upperalpha=%{upperalpha}&loweralpha=%{loweralpha}&unique=%{unique}&format=%{format}&rnd=%{rnd}"
-
-    # Sequence
-    SEQUENCE_QUERY_KEYS = [:min, :max, :col, :format, :rnd]
-    SEQUENCE_QUERY_TEMPLATE = "sequences/?min=%{min}&max=%{max}&col=%{col}&format=%{format}&rnd=%{rnd}"
-
+    QUERY_TEMPLATE = {
+      integer: {
+        path: '/integers',
+        query_keys: [:num, :min, :max, :col, :base, :format, :rnd]
+      },
+      string: {
+        path: '/strings',
+        query_keys: [:num, :len, :digits, :upperalpha, :loweralpha, :unique, :format, :rnd]
+      },
+      sequence: {
+        path: '/sequences',
+        query_keys: [:min, :max, :col, :format, :rnd]
+      }
+    }
   end
 end
--- spec/random_org_http_api/generator_spec.rb
+++ spec/random_org_http_api/generator_spec.rb
@@ -5,14 +5,6 @@ describe RandomOrgHttpApi::Generator do
     @generator = RandomOrgHttpApi::Generator.new
   end
 
-  describe '#insert_params' do
-    it 'should do insert params in string' do
-      params = {name: 'World'}
-      string = 'Hello %{name}'
-      expect(@generator.send(:insert_params, params, string)).to eq('Hello World')
-    end
-  end
-
   describe '#permit_params' do
     it 'should do permit only required params' do
       params = {a: 'A', b: 'B', c: 'C'}
@@ -22,42 +14,8 @@ describe RandomOrgHttpApi::Generator do
   end
 
   describe '#query' do
-    it 'should do generate right query for integer' do
-      object_type = 'integer'
-      params = {num: 4, max: 100}
-      right_query = 'integers/?num=4&min=1&max=100&col=1&base=10&format=plain&rnd=new'
-      expect(@generator.send(:query, object_type, params)).to eq(right_query)
-    end
-
-    it 'should do generate right query for string' do
-      object_type = 'string'
-      params = {min: 1, max: 10, num: 3, len: 5}
-      right_query = 'strings/?num=3&len=5&digits=on&upperalpha=on&loweralpha=on&unique=on&format=plain&rnd=new'
-      expect(@generator.send(:query, object_type, params)).to eq(right_query)
-    end
-
-    it 'should do generate right query for sequence' do
-      object_type = 'sequence'
-      params = {num: 5, len: 10, max: 10}
-      right_query = 'sequences/?min=1&max=10&col=1&format=plain&rnd=new'
-      expect(@generator.send(:query, object_type, params)).to eq(right_query)
-    end
   end
 
   describe '#request' do
-    domain = 'https://www.random.org'
-
-    it 'should do return string with error message if request have bad response' do
-      bad_query = 'strings/?num=HelloWorld&len=5&digits=on&upperalpha=on&loweralpha=on&unique=on&format=plain&rnd=new'
-      expect(@generator.send(:request, domain, bad_query).class).to eq(String)
-    end
-
-    it 'should do return array if request have good response' do
-      if @generator.quota > 0
-        good_query = 'strings/?num=3&len=5&digits=on&upperalpha=on&loweralpha=on&unique=on&format=plain&rnd=new'
-        expect(@generator.send(:request, domain, good_query).class).to eq(Array)
-      end
-    end
   end
-
 end
